e4a7ba3c19bcc5392d88857c907069e8666a22f8
Carlos Herrera <herreradiaz14@gmail.com>
2023-02-09 00:05:20 -0500

    Ajustes del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
+import { ListaAlumnosComponent } from './componentes/lista-alumnos/lista-alumnos.component';
+import { AbmAlumnosComponent } from './componentes/abm-alumnos/abm-alumnos.component';
+import {MaterialModule} from "./material.module";
+import { NombreApellidoPipe } from './pipes/nombre-apellido.pipe';
+import { BooleanTextoPipe } from './pipes/boolean-texto.pipe';
+import { DirectivaPrincipalDirective } from './directivas/directiva-principal.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    ListaAlumnosComponent,
+    AbmAlumnosComponent,
+    NombreApellidoPipe,
+    BooleanTextoPipe,
+    DirectivaPrincipalDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/alumno.ts
@@ -0,0 +1,7 @@
+export interface Alumno{
+  nombre: string;
+  apellido: string;
+  correo: string;
+  edad: number;
+  estaMatriculado: boolean;
+}
